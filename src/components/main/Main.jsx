a01b2caefd42972393778fe814125972dc74a041
Juan <juaniessi@gmail.com>
2022-04-01 17:03:39 -0300

    adding function to setList

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -53,9 +53,14 @@ function Main() {
 	const [seccion, setSeccion] = useState(sectionSusp[0]); // idem seteo
 	const [descripcion, setDescripcion] = useState(descAlin[0]);
 	const [severidad, setSeveridad] = useState('Moderado');
+	const [lista, setlista] = useState([]);
 
 	let section; //esta variable almacena el array de seccion a mapear
 	let description; //esta varibale almacena el array de descripción a mapear
+
+	let listaCompleta = [];
+
+	let armarLista = () => listaCompleta.concat(lista);
 
 	switch (grupo) {
 		// el primer case no es necesario
@@ -192,105 +197,118 @@ function Main() {
 	}
 
 	return (
-		<form>
-			<div class="form-check">
-				<input
-					type="radio"
-					class="form-check-input"
-					name="tipo"
-					id="auto"
-					value="auto"
-					onClick={(e) => setTipo(e.target.value)}
-				/>
-				<label class="form-check-label" for="auto">
-					Auto
-				</label>
-				<input
-					type="radio"
-					class="form-check-input"
-					name="tipo"
-					id="moto"
-					value="moto"
-					onClick={(e) => setTipo(e.target.value)}
-				/>
-				<label for="moto">Moto</label>
+		<div>
+			<form>
+				<div class="form-check">
+					<input
+						type="radio"
+						class="form-check-input"
+						name="tipo"
+						id="auto"
+						value="auto"
+						onClick={(e) => setTipo(e.target.value)}
+					/>
+					<label class="form-check-label" for="auto">
+						Auto
+					</label>
+					<input
+						type="radio"
+						class="form-check-input"
+						name="tipo"
+						id="moto"
+						value="moto"
+						onClick={(e) => setTipo(e.target.value)}
+					/>
+					<label for="moto">Moto</label>
+				</div>
+				<div class="severity-class">
+					<input
+						type="radio"
+						class="severity-class"
+						name="severity"
+						id="leve"
+						value="leve"
+						onClick={(e) => setSeveridad(e.target.value)}
+					/>
+					<label class="severity-class" for="leve">
+						Leve
+					</label>
+					<input
+						type="radio"
+						class="severity-class"
+						name="severity"
+						id="moderado"
+						value="moderado"
+						onClick={(e) => setSeveridad(e.target.value)}
+					/>
+					<label class="severity-class" for="moderado">
+						Moderado
+					</label>
+					<input
+						type="radio"
+						class="severity-class"
+						name="severity"
+						id="grave"
+						value="grave"
+						onClick={(e) => setSeveridad(e.target.value)}
+					/>
+					<label class="severity-class" for="grave">
+						Grave
+					</label>
+				</div>
+				<div class="drop-downs">
+					<label for="group">Grupo</label>
+					<select
+						name="group"
+						id="group"
+						onChange={(e) => setGrupo(e.target.value)}>
+						{group.map((item, i) => (
+							<option key={i} value={item}>
+								{item}
+							</option>
+						))}
+					</select>
+					<label for="section">Sección</label>
+					<select
+						name="section"
+						id="section"
+						onChange={(e) => setSeccion(e.target.value)}>
+						{section.map((item, i) => (
+							<option key={i} value={item}>
+								{item}
+							</option>
+						))}
+					</select>
+					<label for="description">Descripción</label>
+					<select
+						name="description"
+						id="description"
+						onChange={(e) => setDescripcion(e.target.value)}>
+						{description.map((item, i) => (
+							<option key={i} value={item}>
+								{item}
+							</option>
+						))}
+					</select>
+				</div>
+				<div>valor de tipo:"{tipo}"</div>
+				<div>valor de severidad:"{severidad}"</div>
+				<div>valor de grupo: "{grupo}"</div>
+				<div>valor de seccion: "{seccion}"</div>
+				<div>valor de descripcion: "{descripcion}"</div>
+				<div></div>
+			</form>
+			<div>
+				<button
+					onClick={() =>
+						setlista(armarLista, [grupo + '/' + seccion + '/' + descripcion])
+					}>
+					Enviar
+				</button>
 			</div>
-			<div class="severity-class">
-				<input
-					type="radio"
-					class="severity-class"
-					name="severity"
-					id="leve"
-					value="leve"
-					onClick={(e) => setSeveridad(e.target.value)}
-				/>
-				<label class="severity-class" for="leve">
-					Leve
-				</label>
-				<input
-					type="radio"
-					class="severity-class"
-					name="severity"
-					id="moderado"
-					value="moderado"
-					onClick={(e) => setSeveridad(e.target.value)}
-				/>
-				<label class="severity-class" for="moderado">
-					Moderado
-				</label>
-				<input
-					type="radio"
-					class="severity-class"
-					name="severity"
-					id="grave"
-					value="grave"
-					onClick={(e) => setSeveridad(e.target.value)}
-				/>
-				<label class="severity-class" for="grave">
-					Grave
-				</label>
-			</div>
-			<div class="drop-downs">
-				<label for="group">Grupo</label>
-				<select
-					name="group"
-					id="group"
-					onChange={(e) => setGrupo(e.target.value)}>
-					{group.map((item, i) => (
-						<option key={i} value={item}>
-							{item}
-						</option>
-					))}
-				</select>
-				<label for="section">Sección</label>
-				<select
-					name="section"
-					id="section"
-					onChange={(e) => setSeccion(e.target.value)}>
-					{section.map((item, i) => (
-						<option key={i} value={item}>
-							{item}
-						</option>
-					))}
-				</select>
-				<label for="description">Descripción</label>
-				<select
-					name="description"
-					id="description"
-					onChange={(e) => setDescripcion(e.target.value)}>
-					{description.map((item, i) => (
-						<option key={i} value={item}>
-							{item}
-						</option>
-					))}
-				</select>
-			</div>
-			<div>valor de tipo:"{tipo}"</div>
-			<div>valor de severidad:"{severidad}"</div>
-			<div>valor de grupo: "{grupo}"</div>
-			<div>valor de seccion: "{seccion}"</div>
-			<div>valor de descripcion: "{descripcion}"</div>
-		</form>
+			<div>Lista mágica</div>
+			<div>{listaCompleta}</div>
+		</div>
 	);
 }
 
