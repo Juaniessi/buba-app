0ffd1d63b88887e7da1404c1e1c1b614328c1c6c
Juan <juaniessi@gmail.com>
2022-05-30 14:08:41 -0300

    changer brake performance values

--- src/components/main/Report.jsx
+++ src/components/main/Report.jsx
@@ -67,7 +67,7 @@ function Report(props) {
 	 * @constant minorOrEqArray is a MAP containing the key and values to generate the evaluation.
 	 */
 	const minorOrEqArray = new Map([
-		['brakePerf', [10, 40, 45]],
+		['brakePerf', [10, 40, 48]],
 		['handBrakePerf', [1, 14.4, 17]],
 		['brakeStrenght', [0.1, 0.3, 0.5]],
 	]);
