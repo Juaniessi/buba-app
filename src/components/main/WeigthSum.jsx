135b7a98deb54b51723d328ea4c7b14e69740ade
Juan Iessi <juaniessi@gmail.com>
2023-03-31 10:35:48 -0300

    defined new variables

--- src/components/main/WeigthSum.jsx
+++ src/components/main/WeigthSum.jsx
@@ -2,6 +2,48 @@ import React from 'react';
 
 function WeightSum(props) {
 	const {tipo, txtRender} = props;
+
+	function copyToClipboard(text) {
+		if (txtRender !== '') {
+			navigator.clipboard
+				.writeText(text)
+				.then(() => {
+					console.log(`Copied text to clipboard: ${text}`);
+				})
+				.catch((error) => {
+					console.error(`Could not copy text: ${error}`);
+				});
+		}
+	}
+
+	let frontLW =
+		window.fileAsObject !== undefined && txtRender !== ''
+			? Math.round(
+					Number(window.fileAsObject.suspensionEjeDelantero.pesoLadoIzquierdo)
+			  )
+			: 0;
+
+	let frontRW =
+		window.fileAsObject !== undefined && txtRender !== ''
+			? Math.round(
+					Number(window.fileAsObject.suspensionEjeDelantero.pesoLadoDerecho)
+			  )
+			: 0;
+
+	let rearLW =
+		window.fileAsObject !== undefined && txtRender !== ''
+			? Math.round(
+					Number(window.fileAsObject.suspensionEjeTrasero.pesoLadoIzquierdo)
+			  )
+			: 0;
+
+	let rearRW =
+		window.fileAsObject !== undefined && txtRender !== ''
+			? Math.round(
+					Number(window.fileAsObject.suspensionEjeTrasero.pesoLadoDerecho)
+			  )
+			: 0;
+
 	return (
 		<main>
 			{tipo === 'Moto' &&
@@ -44,10 +86,7 @@ function WeightSum(props) {
 			window.fileAsObject !== undefined &&
 			txtRender !== '' ? (
 				<div className="weightsTable">
-					<div>
-						Combustible:{' '}
-						{window.fileAsObject.header.subModelo}
-					</div>
+					<div>Combustible: {window.fileAsObject.header.subModelo}</div>
 					<div>
 						Peso total:{' '}
 						{Math.round(
